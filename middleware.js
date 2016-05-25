0c9d7228d8803e113d0038781f6678ed63c7afc5
josealvarado <josealvarado@Joses-MacBook-Pro.local>
2016-05-24 17:41:08 -0700

    Refactor middleware

---
+++ middleware.js
@@ -0,0 +1,15 @@
+// Use module exports to exporse middileware
+
+var middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function(req, res, next) {
+		// new Date().toString();
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+}
+
+module.exports = middleware;
